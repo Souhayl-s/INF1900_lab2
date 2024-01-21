01b5513128542313eceac4422b516568685142f0
Souhayl <souhaylsejjari1@gmail.com>
2024-01-21 15:47:04 -0500

    update

--- SM_template.cpp
+++ SM_template.cpp
@@ -1,3 +1,19 @@
+/*IDEES*/
+
+/*Fonction couleur*/
+// Faire des fonctions specifiques pour les changements de couleur et indiquer les ports a utiliser dans la description (eteinte , rouge ,vert , ambre)
+// point forts :
+// intuitif pendant le codage, on a pas a reflechir pour le masque a chaque fois -> gain de temps
+// evite les erreurs
+// si on veut changer de port --> il suffit de le changer dans la fonction
+
+/*Class enum pour les etats*/
+// Rouge_Presse --> Rouge bouton Presse
+// Vert_Libre --> Vert bouton LIBRE
+// ...
+
+
+
 /* DESCRIPTION */
 
 
--- pb2/probleme2_v1.cpp
+++ pb2/probleme2_v1.cpp
@@ -6,19 +6,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-// IDEES
 
-/*Fonction couleur*/
-// Faire des fonctions specifiques pour les changements de couleur et indiquer les ports a utiliser dans la description (eteinte , rouge ,vert , ambre)
-// point forts :
-// intuitif pendant le codage, on a pas a reflechir pour le masque a chaque fois -> gain de temps
-// evite les erreurs
-// si on veut changer de port --> il suffit de le changer dans la fonction
-
-/*Class enum pour les etats*/
-// Rouge_Presse --> Rouge bouton Presse
-// Vert_Libre --> Vert bouton LIBRE
-// ...
 
 // PSEUDO - CODE //
 
