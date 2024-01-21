c7ca132ba6cedc18c61a4c9f7736d282288aedab
Souhayl <souhaylsejjari1@gmail.com>
2024-01-21 11:19:44 -0500

    version du probleme avec 2 etats seulement ajoutee

--- pb1/probleme1_version_2.cpp
+++ pb1/probleme1_version_2.cpp
@@ -1,27 +1,12 @@
 /*MACHINE A ETAT*/
 
-// 4 ETATS
+// 2 ETATS
 
 
 #define F_CPU 8000000UL // 8 MHz
 #include <avr/io.h>
 #include <util/delay.h>
 
-/* Notes*/
-
-// legende : BPR = bouton presse relache
-// Seulement besoin de mettre l'etat eteint pour l'etat initial car la couleur change seulement a l'etat allume pendant 2 secondes
-
-// Couleur-actuel = SORTIE_actuel en terme de machine a etat = on va utiliser PORT
-// BPR = Input actuel en terme de machine a etat  = on va utiliser PIN
-
-// LED :
-// A2 et A3  --> 01 = rouge et 10 = vert
-// Mettre A2 et A3 en mode sortie
-
-// bouton :
-// D2 : 0 -> bouton non presse / 1 -> bouton presse
-// mettre D2 en mode entree
 
 /*Code*/
 
@@ -33,11 +18,9 @@ bool ButtonPressed_then_Released();
 // Definition d'une classe Enum
 enum etat_Robot
 {
-    etat_Eteint_0,
-    etat_Eteint_1,
-    etat_Eteint_2,
+    etat_Eteint,
     etat_Allume
-}; // le nombre apres 'eteint' fait reference au nombre de fois que le bouton a ete presse et relache
+}; 
 
 // Definition de constantes de couleur
 // Definition des masques pour les couleurs de LED :
@@ -58,49 +41,31 @@ int main()
 {
     // Definition de chaque port en mode entree ou sortie
 
-    // DDRA |= (1 << PA3) | (1 << PA2); // PORT de la LED A3 et A2 en sortie
-    // DDRD &= ~(1 << PD2);             // PORT pour le bouton poussoir D2 en entree    
-
-    DDRA = 0xff ; 
-    DDRD = 0x00 ; 
-
+    DDRA |= (1 << PA3) | (1 << PA2); // PORT de la LED A3 et A2 en sortie
+    DDRD &= ~(1 << PD2);             // PORT pour le bouton poussoir D2 en entree    
 
     // etat_actuel =  eteint_0  (etat initial)
-    etat_Robot etat_present{etat_Eteint_0};
-    // Boucle infinie = while true
+    etat_Robot etat_present{etat_Eteint};
+
+    // Initialisation du compteur de l'operation (Pression -> Relache) du bouton
+    int8_t compteur {0} ;
+
     while (true)
     {
         switch (etat_present)
         {
-        // Si etat_actuel est 'eteint0' :
-        case etat_Eteint_0:
+        case etat_Eteint:
         {
             // Couleur_actuel = LED_ETEINTE
             PORTA |= (masque_DEL_eteinte);
             // Si BPR :
             bool est_presse_relache{ButtonPressed_then_Released()};
-            if (est_presse_relache)
-                // passer a l'etat 'eteint1
-                etat_present = etat_Eteint_1;
+            if (est_presse_relache){
+                compteur += 1;
+                if (compteur == 3)
+                    etat_present = etat_Allume ; 
+            }
             break ; 
-        }
-        case etat_Eteint_1:
-        {
-            // Si BPR :
-            bool est_presse_relache{ButtonPressed_then_Released()};
-            if (est_presse_relache)
-                // passer a l'etat 'eteint2'
-                etat_present = etat_Eteint_2;
-            break ;                             
-        }
-        case etat_Eteint_2:
-        {
-            // Si BPR :
-            bool est_presse_relache{ButtonPressed_then_Released()};
-            if (est_presse_relache)
-                // passer a l'etat 'Allume'
-                etat_present = etat_Allume;
-            break ;                 
         }
         case etat_Allume:
         {
@@ -110,7 +75,9 @@ int main()
 
             _delay_ms(delai_DEL_Verte);
             // revenir a l'etat initial
-            etat_present = etat_Eteint_0;
+            etat_present = etat_Eteint;
+            // reinitialisation du compteur 
+            compteur = 0 ; 
             break ;             
         }
         }
